7172c168d05264ee478e32c50590ba35c2304d3f
Wendell Smith <wackywendell@gmail.com>
2015-07-12 14:58:40 -0400

    Added plotting of fractions

--- examplescripts/uvplotter.py
+++ examplescripts/uvplotter.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 dr = 'PyCORN'
 if dr not in sys.path: sys.path.append(dr)
@@ -33,8 +34,31 @@ parser.add_argument("--ymax2", default=None, type=float,
                     help = "Upper bound on the right y-axis")
 parser.add_argument("--dpi", default=None, type=int,
                     help = "DPI (dots per inch) for raster images (png, jpg, etc.)")
+parser.add_argument("--no_fractions",
+                    dest="fractions",
+                    help="Disable plotting of fractions",
+                    action = "store_false")
 
 args = parser.parse_args()
+
+def mapper(min_val, max_val, perc):
+    '''
+    calculate relative position in delta min/max
+    '''
+    x = abs(max_val - min_val) * perc
+    if min_val < 0:
+        return (x - abs(min_val))
+    else:
+        return (x + min_val)
+
+
+def expander(min_val, max_val, perc):
+    '''
+    expand -/+ direction of two values by a percentage of their delta
+    '''
+    delta = abs(max_val - min_val)
+    x = delta * perc
+    return (min_val - x, max_val + x)
 
 for fname in args.inp_res:
     path = Path(fname)
@@ -51,14 +75,30 @@ for fname in args.inp_res:
     xmax = max(UVx) if args.xmax is None else args.xmax
     condx, cond = np.array(pc['Cond']['data']).T
     
+    if args.fractions:
+        frac_data = pc['Fractions']['data']
+        frac_x, frac_y = list(zip(*frac_data))
+        frac_delta = [abs(a - b) for a, b in zip(frac_x, frac_x[1:])]
+        frac_delta.append(frac_delta[-1])
+        
+        if args.xmin is None: xmin = min(frac_x)
+        if args.xmax is None: xmax = frac_x[-1] + frac_delta[-1]
+    
     for name in 'general', 'sizeex':
-        fig, ax = plt.subplots(figsize=(4,3))
+        if args.fractions:
+            fig = plt.figure(figsize=(8, 6)) 
+            gs1, gs2 = gridspec.GridSpec(2, 1, height_ratios=[9, 1], hspace=0) 
+            ax = plt.subplot(gs1)
+            ax_fracs = plt.subplot(gs2, sharex=ax)
+        else:
+            fig, ax = plt.subplots(figsize=(4,3))
         ax.plot(UVx, UV, color=blue, lw=2)
 
         ax2 = ax.twinx()
         ax2.plot(condx, cond, color=green, lw=2)
-
-        ax.set_xlabel('Elution Volume (ml)')
+        
+        ax_to_label = ax_fracs if args.fractions else ax
+        ax_to_label.set_xlabel('Elution Volume (ml)')
         ax.set_ylabel('Absorbance (mAu)', color=blue)
         ax.set_xlim(xmin, xmax)
         ax.set_ylim(args.ymin, args.ymax)
@@ -78,6 +118,18 @@ for fname in args.inp_res:
             exp = int(np.log10(mx/2.))
             ymax2 = round(mx*2, -exp)*2
         ax2.set_ylim(args.ymin2, ymax2)
+        
+        if args.fractions:
+            for (x, fracname), delta in zip(frac_data, frac_delta):
+                ax_fracs.axvline(x=x, color='r', linewidth=0.85)
+                ax_fracs.annotate(str(fracname), xy=(x + delta * 0.55, 0.1),
+                         horizontalalignment='center', verticalalignment='bottom', size=8, rotation=90)
+            ax_fracs.set_ylim(0, 1)
+            ax_fracs.set_yticks([])
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_xticklines(), visible=False)
+            ax_fracs.get_xaxis().set_tick_params(
+                which='both', direction='out', top='off')
         
         outputpath = path.with_name('{}-{}.{}'.format(
                     path.stem, name, args.ext))
